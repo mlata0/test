26a0de98e03f86c818cdc93f52028160460a94c1
Mateusz <lata.mateusz92@gmail.com>
2024-04-23 20:10:58 +0200

    cwiczenie tworzenie zbiorow danych

---
+++ data.py
@@ -0,0 +1,21 @@
+
+pracownicy = ["tomasz", "Jan", "Kamil"]
+
+print(pracownicy)
+
+nazwiska = ("Kowalski", "Benedytky" "Klos")
+
+print(nazwiska)
+
+zbior_imion = {"tomasz", "Roman", "Leszek"}
+zbior_imion2 = {"tomasz", "karol", "zenek"}
+
+print(zbior_imion)
+print(zbior_imion2)
+
+slowik = { 
+"imie": "Janek",
+"age": 17
+}
+
+print(slowik)
